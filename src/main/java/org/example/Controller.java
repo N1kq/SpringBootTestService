541836ff4f0422de8637329d9470b234798a5c34
N1kq <53380173+N1kq@users.noreply.github.com>
2023-12-09 10:34:36 +0300

    Get запрос books

--- src/main/java/org/example/Controller.java
+++ src/main/java/org/example/Controller.java
@@ -7,13 +7,18 @@ import java.util.*;
 @RestController
 @RequestMapping("/bookstore")
 public class Controller {
-    List<String> books = Arrays.asList("Sherlock Holmes", "Arsene Lupin", "Code da Vinci");
+    List<String> books = new ArrayList<>(Arrays.asList("Sherlock Holmes", "Arsene Lupin", "Code da Vinci"));
 
-    @GetMapping()
+
+    @GetMapping("/books")
     public BookList bookList(){
         return new BookList(books);
     }
 
-    //@PostMapping("/postNewBook")
+    @PostMapping("/NewBook")
+    public String newBook(@RequestParam(value = "book") String book){
+        books.add(book);
+        return "New book added successfully";
+    }
 
 }
